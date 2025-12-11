376bd5a6f4fa7e489506a407478b424a4d7677b9
Mukesh Sepat <mukeshjat83024@gmail.com>
2025-12-11 16:25:03 +0530

    this is database connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const connectedDatabase = require("../server/src/config/db")
+const adminRouter = require("../server/src/routes/addadminRoute")
 const app = express()
 const port = 4001
+connectedDatabase()
+
+app.use(express.json())
+app.use('/api/admin',adminRouter)
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`App is running on port : http://localhost:${port}`))
