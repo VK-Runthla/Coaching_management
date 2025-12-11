a1902175339d851a12a1b1beb8b830f1fdb9b98f
rahul-yadav695 <officialrahulyadav961@gmail.com>
2025-12-11 16:28:51 +0530

    add schema subject

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,1 +1,15 @@
-// this is model.js
+const mongoose = require('mongoose')
+
+const SubjectSchema = new mongoose.Schema({
+    subject_name: {
+        type: String,
+        required: true
+    },
+    Course: {
+        type: String,
+        required: true
+    }
+})
+
+const Subject = mongoose.model('subject', SubjectSchema)
+module.exports = Subject
