73051e616fbdd1c462066b12b31e4cddcc55165b
rahul-yadav695 <officialrahulyadav961@gmail.com>
2025-12-16 15:42:49 +0530

    subject add

--- server/index.js
+++ server/index.js
@@ -4,16 +4,13 @@ const adminRouter = require("../server/src/routes/addadminRoute");
 const routerCourse = require("./src/routes/academics/courseRouter");
 const subjectRouter = require("./src/routes/academics/Subject");
 
-const subjectRouter = require("./src/routes/SubjectRoutes");
 const app = express();
 const port = 4001;
 connectedDatabase();
 app.use(express.json());
 app.use("/api/subject", subjectRouter);
-
 app.use("/api/admin", adminRouter);
-app.use("/course", routerCourse);
-app.use("/api/student", subjectRouter);
+app.use("/course", routerCourse); 
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () =>
