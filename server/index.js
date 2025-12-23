77a3a7f6048e0038ce1ad1c262e934335fc6b197
rahul-yadav695 <officialrahulyadav961@gmail.com>
2025-12-23 15:48:25 +0530

    Advertisement work

--- server/index.js
+++ server/index.js
@@ -7,12 +7,14 @@ const sessionrout = require("./src/routes/sessionRout");
 const adminRouter = require("./src/routes/adminauthRoutes/addadminRoute");
 // const subjectRouter = require("./src/routes/SubjectRoutes");
 const popularCourseRoute = require('./src/routes/popularCourseRoute/popularCourseRoute');
+const SchoolImg = require("./src/routes/Advertisement/school");
 
 const app = express();
 const port = 4001;
 
 connectedDatabase();
 app.use(express.json());
+app.use('/api/school' , SchoolImg)
 app.use("/api/subject", subjectRouter);
 app.use("/api/admin", adminRouter);
 app.use("/course", routerCourse);
